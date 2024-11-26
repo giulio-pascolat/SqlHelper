38e7d07626f02f4970a8533326c2ac1fd01136ac
giulio pascolat <Giuliopascolat@gmail.com>
2024-11-26 17:41:32 +0100

    added Dapper

--- SqlClient/Database.cs
+++ SqlClient/Database.cs
@@ -1,7 +1,7 @@
-using System.Data;
 using Microsoft.Data.SqlClient;
 using SqlClient.Domain;
 using SqlClient.SeedWork;
+using Dapper;
 
 namespace SqlClient;
 
@@ -12,65 +12,34 @@ public class Database(string connectionString) : IDatabase
     public IEnumerable<MyNote> Read()
     {
         const string query = "SELECT Id, Note, Inserted FROM Notes";
-
         connection.Open();
-        
-        using var command = new SqlCommand(query, connection);
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            yield return new(
-                reader.GetInt32(0),
-                reader.GetString(1),
-                reader.GetDateTimeOffset(2)
-            );
-        }
-        
+        var result = connection.Query<MyNote>(query);
         connection.Close();
+        return result;
     }
 
     public void Insert(string note)
     {
         const string query = "INSERT INTO Notes (Note, Inserted) VALUES (@Note, @Inserted)";
-
         connection.Open();
-        
-        using var command = new SqlCommand(query, connection);
-        command.Parameters.AddWithValue("@Note", note);
-        command.Parameters.AddWithValue("@Inserted", DateTimeOffset.Now);
 
-        command.ExecuteNonQuery();
-        
+        connection.Execute(query, new { Note = note, Inserted = DateTime.Now });
         connection.Close();
     }
 
     public void Update(int id, string note)
     {
         const string query = "UPDATE Notes SET Note = @Note WHERE Id = @Id";
-
         connection.Open();
-        
-        using var command = new SqlCommand(query, connection);
-        command.Parameters.AddWithValue("@Id", id);
-        command.Parameters.AddWithValue("@Note", note);
-
-        command.ExecuteNonQuery();
-
+        connection.Execute(query, new { Id = id, Note = note });
         connection.Close();
     }
 
     public void Delete(int id)
     {
         const string query = "DELETE FROM Notes WHERE Id = @Id";
-
         connection.Open();
-
-        using var command = new SqlCommand(query, connection);
-        command.Parameters.AddWithValue("@Id", id);
-
-        command.ExecuteNonQuery();
-        
+        connection.Execute(query, new { Id = id });
         connection.Close();
     }
 
@@ -80,30 +49,25 @@ public class Database(string connectionString) : IDatabase
 
         connection.Open();
         var time = DateTimeOffset.Now;
-        
+
         var transaction = connection.BeginTransaction();
-        using var command = new SqlCommand(query, connection,transaction);
-        
+
         try
         {
-            command.Parameters.Add("@Note", SqlDbType.NVarChar);
-            command.Parameters.Add("@Inserted", SqlDbType.DateTimeOffset);
             foreach (var note in notes)
             {
-                command.Parameters["@Note"].Value = note;
-                command.Parameters["@Inserted"].Value = time;
-                command.ExecuteNonQuery();
+                connection.Execute(query, new { Note = note, Inserted = time }, transaction: transaction);
             }
-            
             transaction.Commit();
         }
         catch (Exception e)
         {
             transaction.Rollback();
         }
-       
-        connection.Close();
-        
+        finally
+        {
+            connection.Close();
+        }
     }
 
     public async ValueTask DisposeAsync() => await connection.DisposeAsync();
